7628528ed24bf4fb136c4b76da91df3fd7a647ee
eduardouio <accxell_ax3c@hotmail.es>
2013-01-25 15:13:14 -0500

    terminado el modelo falta implementar trasacciones

--- aplicacion/reporte/modelo.py
+++ aplicacion/reporte/modelo.py
@@ -45,12 +45,12 @@ class Model(object):
 		sql = QtSql.QSqlQuery()				
 
 		sql.prepare('SHOW TABLES FROM slnecc_control;')
-		resultado = self.consultDb(sql)
+		result = self.__consultDb(sql)
 
-		if not resultado:			
-			return resultado
+		if not result:			
+			return False
 
-		return False
+		return result
 
 
 	def listColumns(self,tabla):
@@ -58,12 +58,12 @@ class Model(object):
 		sql = QtSql.QSqlQuery()					
 		
 		sql.prepare("SHOW COLUMNS FROM " + tabla + " ;")
-		resultado = self.consultDb(sql)
+		result = self.__consultDb(sql)
 
-		if not resultado:			
-			return resultado
+		if not result:			
+			return False
 
-		return False
+		return result
 
 	def getQueryModel(self,columns,table):
 		'''Retorna un modelo de solo lectura de una tabla
@@ -106,114 +106,167 @@ class Model(object):
 
 
 	def selectQuery(self, table, columns ,condition, like, limit):
-		'''
-		Ejecuta una consulta tipo SELECT en la BD
+		'''Ejecuta una consulta tipo SELECT en la BD
 
 		(str)	table 		=>	nombre de la tabla a consultar
 		(list)	columns 	=>	Columnas a mostrar
 		(str)	condition 	=> 	condicion si no existe "1=1"
-		(str)	like		=>	para filtras busquedas de no existir es False
+		(str)	like		=>	para filtras busquedas de no existir es False (columna = valor)
 		(int)	limit		=>	limite de registros si se desa la tabla completa vale 0
 
 		SELECT columns FROM table
 		WHERE conditions | like
-		LIMIT limit 
-		'''
+		LIMIT limit | nothing
+		'''		
+		query = 'SELECT '
+		# x(desde) i(hasta)
+		x = 1 
+		i = len(columns)
+		for item in columns:
+			if x < i:
+				query = query + item + ','
+			if x == i:
+				query = query + item + ' FROM ' + table
+			x+=1
 
-		
+		#analizamos la condicion
+		query = query + ' WHERE ' 
 
-		sql = QtSql.QSqlQuery()
+		if not condition and not Like:
+			query = query + '1=1'
+		elif condition and like :
+			query = query + condition + ' ' + like
+		elif not condition and like:
+			query = query + like
+		elif not like and condition:
+			query = query + condition
 
-		if (condition == False)
-			sql.prepare("SELECT * FROM " + table + ";")
+		#terminamos de armar la consulta
+		if limit == 0:
+			query = query + ';'
 		else:
-			sql.prepare("SELECT * FROM " + table +  condition + ";")	
-			
-		resultado = self.consultDb(sql)
+			query = query + ' LIMIT ' limit + ';'
 
-		if not resultado:
-			return resultado
-
-		return False
-
-
-	
-	def insertTable(self,table,row):
-		'''
-		Inserta un registro en la DB
-
-		table 		=>	nombre de la tabla
-		row 	=>	diccionario de calve valor (columna/valor)
-
-		'''
-		#separo en una lista cada valor
-		columnas = []
-		valores = []
-		for item in diccionario:
-			columnas.append(item)
-			valores.append(diccionario[item])
-
-		#Preparo la primera parte de la sentencia
-		misql = "INSERT INTO " + table + "("
-		i = 1
-		x = len(columnas)
-		for columna in columnas:
-			if (i<x):
-				
 		sql = QtSql.QSqlQuery()
-		sql.prepare("INSERT INTO `slnecc_control`.`cargo_sln`"
-					"(`id_cargo_sln`,"
-					"`cargo`,"
-					"`descripcion`,"
-					"`create`,"
-					"`last_update`)"
-					"VALUES"
-					"("
-					"<{id_cargo_sln: }>,"
-					"<{cargo: }>,"
-					"<{descripcion: }>,"
-					"<{create: }>,"
-					"<{last_update: CURRENT_TIMESTAMP}>"
-					");"
-					)
+		sql.prepare(query)
+		#ejecutamos la consulta, si hay un error acudir a last error
+		result = self.__consultDb(sql)
 
-		columnas = []
-		valores = []
+		if not result:			
+			return False
 
+		return result
 
-		for item in diccionario:
-			columnas.append(item)
-			valores.append(diccionario[item])
+	def insertQuery(self,table,values):
+		'''Ejecuta una consulta tipo INSERT en la BD, si se manda una columna sin valor se reemplaza por NULL
+		(str) table 	=>	nombre de la tabla 
+		(dic) values 	=>  diccionario clave valor 
 
-
-	def updateTable(self,table,columns):
+		INSERT INTO table (values[columns]) 
+		VALUES( values[value]);
 		'''
-		Edita un registro de una tabla
+		
+		query = 'INSERT INTO ' table +'('
 
-		table 		=>	nombre de la tabla
-		columns 	=>	diccionario de calve valor
+		i = 1
+		x = len(values)
+		for item in values:
+			if i<x:
+				query = query + item + ','
+			if i==x:
+				query = query + item + ')'
+			i+=1
+
+		query = query + 'VALUES('
+
+		i = 1
+		for item in values:
+			if values[item] == '':
+				#si el valor esta vacio se escribe NULL
+				values[item] = 'NULL'
+			if i < x:
+				query = query + values[item] + ','
+
+			if i == x:
+				query = query + values[item] + ');'
+			i +=1
+
+		sql = QtSql.QSqlQuery()
+		sql.prepare(query)
+
+		result = self.__consultDb(sql)
+
+		if not result:
+			return False
+
+		return result
+
+
+	def updateQuery(self,table,values,condition):
+		'''Ejecuta una Sentencia tipo update en la BD
+
+		(str)	table 	=> nombre de la tabla 
+		(dic)	values 	=> diccionario clave valor para update
+		(srt)	condition => condicion SQL
+
+		UPDATE table 
+		SET
+		values[columns] = values[value]
 		'''
 
-			
-		pass
+		query = 'UPDATE ' + table + ' SET '
+		i = 1
+		x = len(values)
 
-	def deleteTable(self,table,conditions):
-		'''
-		Elimina un registr de una tabla
-		table 		=>	nombre de la tabla
-		conditions 	=>	condiciones (clave valor)		
-		'''
-		pass
+		#armamos la consulta
+		for item in values:
+			if values[item] == '':
+				#si el valor esta vacio se escribe NULL
+				values[item] = 'NULL'
+			if i < x :
+				query = query + item + ' = ' + values[item]	+ ','
+			if i == x :
+				query = query + item + ' = ' + values[item]
+		query = query  + ' ' + condition + ';'
 
-	def lastQuery(self):
-		'''	retorna el Sql de la última consulta'''
-		pass
+		sql = QtSql.QSqlQuery()
+		sql.prepare(query)
+		result = self.__consultDb(sql)
+
+		if not result:
+			result False
+
+		return result
+
+
+	def deleteQuery(self, table, condition ):
+		'''Metodo encargad de ejecutar una Sentencia tipo DELETE en la BD
+		(str)	table 	=> nombre de la tabla
+		(str)	condition => condicion para el borrado
+
+		DELETE FOM table
+		WHERE condition
+		'''
+		sql = QtSql.QSqlQuery()		
+		sql.prepare('DELETE FROM ' + table + ' WHERE ' + condition + ';')
+
+		result = sefl.__consultDb(sql)
+
+		if not result:
+			return False
+
+		return result
+
 
 	def lastInsertId(self):
 		'''Ultimo Id ingresado en la BD'''
-		pass
+		sql = QtSql.QSqlQuery()
+		return sql.lastInsertId()
 
-
+	def lastQuery(self):
+		'''	retorna el Sql de la última consulta'''
+		sql = QtSql.QSqlQuery()
+		return sql.lastQuery()
 
 
 if __name__ == '__main__':	
